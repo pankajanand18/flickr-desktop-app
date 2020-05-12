3f40095f16424e0a457e05069bd94874c71da292
Pankaj Anand <pankajanand18@gmail.com>
2020-05-11 23:49:36 -0700

    initial changes

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -6,8 +6,8 @@ import styles from './Home.css';
 export default function Home() {
   return (
     <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}>to Counter</Link>
+      <h2>Welcome Home</h2>
+      <Link to={routes.COUNTER}>Go to Counter</Link>
     </div>
   );
 }
