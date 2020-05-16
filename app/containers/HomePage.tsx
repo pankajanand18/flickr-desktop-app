a9ded4d46c861a7f7b68427669a9c1727d45a0b3
Pankaj Anand <pankajanand18@gmail.com>
2020-05-16 14:29:14 -0700

    more changes fro mainApp

--- app/containers/HomePage.tsx
+++ app/containers/HomePage.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import Home from '../components/Home';
+// import Home from '../components/Home';
+// import MyList2 from '../components/learn/MyList2';
+import MainApp from '../components/learn/MainApp';
+// import MyApp3 from '../components/MyApp3';
 
 export default function HomePage() {
-  return <Home />;
+  return <MainApp />;
 }
