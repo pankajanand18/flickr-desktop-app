ee1ddb0d8684997657ed906a4d5ea43717f72123
Pankaj Anand <pankajanand18@gmail.com>
2020-05-16 20:15:35 -0700

    reverted the changes to how the home page

--- app/containers/HomePage.tsx
+++ app/containers/HomePage.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-// import Home from '../components/Home';
 // import MyList2 from '../components/learn/MyList2';
-import MainApp from '../components/learn/MainApp';
+// import MainApp from '../components/learn/MainApp';
+import Home from '../components/Home';
 // import MyApp3 from '../components/MyApp3';
 
 export default function HomePage() {
-  return <MainApp />;
+  return <Home />;
 }
