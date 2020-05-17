b650cb357773318fe339eba9a9205af5b97f1770
Pankaj Anand <pankajanand18@gmail.com>
2020-05-16 18:57:10 -0700

    added a new factory class

---
+++ app/components/learn/factories/ContactCardFactory.tsx
@@ -0,0 +1,37 @@
+// import { Contact } from '../Interfaces';
+
+const contactFactory = [
+  {
+    imageUrl:
+      'https://live.staticflickr.com/65535/49585792327_be9a904a1b_w.jpg',
+    name: 'Pankaj',
+    email: 'pankaj@email.com',
+    phone: '42542342s',
+    id: 1
+  },
+  {
+    imageUrl:
+      'https://live.staticflickr.com/65535/49585792327_be9a904a1b_w.jpg',
+    name: 'Pankaj',
+    email: 'pankaj@email.com',
+    phone: '42542342s',
+    id: 2
+  },
+  {
+    imageUrl:
+      'https://live.staticflickr.com/65535/49585792327_be9a904a1b_w.jpg',
+    name: 'Pankaj',
+    email: 'pankaj@email.com',
+    phone: '42542342s',
+    id: 3
+  },
+  {
+    imageUrl:
+      'https://live.staticflickr.com/65535/49585792327_be9a904a1b_w.jpg',
+    name: 'Pankaj',
+    email: 'pankaj@email.com',
+    phone: '12342542342s',
+    id: 4
+  }
+];
+export default contactFactory;
