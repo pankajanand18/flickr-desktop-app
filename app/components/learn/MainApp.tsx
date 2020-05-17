f43e0cde20cf5d112f86bf7225a30c4d3b4842f2
Pankaj Anand <pankajanand18@gmail.com>
2020-05-16 20:18:20 -0700

    more changes

--- app/components/learn/MainApp.tsx
+++ app/components/learn/MainApp.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import NavBar from './NavBar';
 import contactFactory from './factories/ContactCardFactory';
 import ContactCard from './ContactCard';
+import styles from '../Home.css';
 
 export default function MainApp() {
   const contactCardList = contactFactory.map(value => (
     <ContactCard key={value.id} contact={value} />
   ));
   return (
-    <div className="container">
+    <div className={styles.container} data-tid="container">
       <NavBar />
       <div className="todo-list" />
       {contactCardList}
