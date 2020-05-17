0acaa14eb9f6639b3c2f5d6eab922675bed4afbf
Pankaj Anand <pankajanand18@gmail.com>
2020-05-16 20:17:02 -0700

    a new app to show Contacts

---
+++ app/containers/Contacts.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainApp from '../components/learn/MainApp';
+import HomePageLink from '../components/HomePageLink';
+
+export default function Contacts() {
+  return (
+    <div>
+      <HomePageLink />
+      <MainApp />
+    </div>
+  );
+}
