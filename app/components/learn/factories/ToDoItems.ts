d124c7b16c08d72e68ebda3f7b1447a383c96868
Pankaj Anand <pankajanand18@gmail.com>
2020-05-17 15:21:50 -0700

    Added a new factory for ToDoItems

---
+++ app/components/learn/factories/ToDoItems.ts
@@ -0,0 +1,29 @@
+const todoItems = [
+  {
+    id: 1,
+    taskName: 'Laundary',
+    selected: true
+  },
+  {
+    id: 2,
+    taskName: 'Car wash',
+    selected: true
+  },
+  {
+    id: 3,
+    taskName: 'Baking',
+    selected: true
+  },
+  {
+    id: 4,
+    taskName: 'Youtube',
+    selected: false
+  },
+  {
+    id: 5,
+    taskName: 'Google',
+    selected: false
+  }
+];
+
+export default todoItems;
