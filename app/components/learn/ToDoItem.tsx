f1ef13ffcadd0ce7cc632d49854ba14201c536cd
Pankaj Anand <pankajanand18@gmail.com>
2020-05-17 22:54:08 -0700

    updated the ToDoItem

--- app/components/learn/ToDoItem.tsx
+++ app/components/learn/ToDoItem.tsx
@@ -1,10 +1,42 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
+import { ToDoItemState } from './Interfaces';
 
-export default function ToDoItem() {
-  return (
-    <div className="todo-item">
-      <input type="checkbox" />
-      <p> Placeholder Item</p>
-    </div>
-  );
+type DefaultToDoItem = {
+  value: ToDoItemState;
+  handler: (id: number) => void;
+};
+class ToDoItem extends React.Component<DefaultToDoItem> {
+  constructor(item: DefaultToDoItem) {
+    super(item);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log(this.props.value.taskName);
+  }
+
+  render(): JSX.Element {
+    console.log('rendering item with ', this.props.value.id);
+    return (
+      <div className="todo-item">
+        <input
+          type="checkbox"
+          onClick={() => this.props.handler(this.props.value.id)}
+          checked={this.props.value.selected}
+        />
+        <p>{this.props.value.taskName}</p>
+      </div>
+    );
+  }
 }
+
+ToDoItem.defaultProps = {
+  value: {
+    taskName: 'loading..',
+    selected: false
+  },
+  handler: (_id: number) => {}
+};
+
+export default ToDoItem;
