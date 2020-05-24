19e4634cbbc327b09c58e77a0322c08cca0ab999
Pankaj Anand <pankajanand18@gmail.com>
2020-05-24 11:04:43 -0700

    more changes

--- app/components/learn/StarWarsApp.tsx
+++ app/components/learn/StarWarsApp.tsx
@@ -1,1 +1,48 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
+import HomePageLink from '../HomePageLink';
+import styles from '../Home.css';
+
+type StarWarsAppState = {
+  loading: boolean;
+  name: {};
+};
+export default class StarWarsApp extends React.Component<{}, StarWarsAppState> {
+  constructor() {
+    super({});
+    this.state = {
+      name: {},
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://swapi.dev/api/people/1/')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ name: data.name, loading: false });
+        return data;
+      })
+      .catch(reason => {
+        this.setState({ name: reason.name, loading: false });
+        return reason;
+      });
+  }
+
+  render(): JSX.Element {
+    const text = this.state.loading ? (
+      'loading'
+    ) : (
+      <div className={styles.container} data-tid="container">
+        {this.state.name}
+      </div>
+    );
+    return (
+      <div>
+        <HomePageLink />
+        {text}
+      </div>
+    );
+  }
+}
